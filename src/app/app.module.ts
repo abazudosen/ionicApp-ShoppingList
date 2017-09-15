ac92bc7ac4688868beb8ed79b65ea375873d5f4a
abazudosen <abudosen@gmal.com>
2017-09-15 10:56:16 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,59 @@
+import {HttpModule} from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { MyApp } from './app.component'
+import { TabsPage } from '../pages/tabs/tabs';
+import { ShoppingListPage } from '../pages/shopping-list/shopping-list';
+import { RecipesPage } from '../pages/recipes/recipes';
+import { RecipePage } from '../pages/recipe/recipe';
+import { EditRecipePage } from '../pages/edit-recipe/edit-recipe';
+import { ShoppingListService } from '../services/shopiing-list';
+import { RecipesService } from '../services/recipe';
+import { SignupPage } from '../pages/signup/signup';
+import { SigninPage } from '../pages/signin/signin';
+import { AuthService } from '../services/auth';
+import { DatabaseOptionsPage } from '../pages/database-options/database-options';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    TabsPage,
+    ShoppingListPage,
+    RecipesPage,
+    RecipePage,
+    EditRecipePage,
+    SigninPage,
+    SignupPage,
+    DatabaseOptionsPage
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    TabsPage,
+    ShoppingListPage,
+    RecipesPage,
+    RecipePage,
+    EditRecipePage,
+    SigninPage,
+    SignupPage,
+    DatabaseOptionsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ShoppingListService,
+    RecipesService,
+    AuthService
   ]
 })
 export class AppModule {}
